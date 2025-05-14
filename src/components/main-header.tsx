5de490e8a975af31142e72e59e3c662887dd44e0
ddangmul <j.ddangmul@gmail.com>
2025-05-14 16:09:20 +0900

    레이아웃 수정(sm)

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -6,15 +6,21 @@ import { ArchiveBoxIcon } from "@heroicons/react/24/outline";
 const MainHeader: React.FC = () => {
   return (
     <header className="w-full bg-transparent shadow py-4 fixed top-0 left-0 z-50">
-      <div className="w-full flex items-center justify-between md:px-6 lg:px-60">
-        <div className="flex flex-start md:gap-6 lg:gap-8 items-center">
+      <div className="w-full flex items-center justify-between px-4 md:px-6 lg:px-60">
+        <div className="flex flex-start gap-4 md:gap-6 lg:gap-8 items-center">
           <Link href="/" className="logo" aria-label="로고">
             <Image src={Logo} alt="로고 이미지" width={40} height={20} />
           </Link>
           <div className="flex gap-4 md:text-lg text-[#555555]">
-            <Link href="/" aria-label="home">홈</Link>
-            <Link href="/movie" aria-label="영화">영화</Link>
-            <Link href="/series" aria-label="시리즈">시리즈</Link>
+            <Link href="/" aria-label="home">
+              홈
+            </Link>
+            <Link href="/movie" aria-label="영화">
+              영화
+            </Link>
+            <Link href="/series" aria-label="시리즈">
+              시리즈
+            </Link>
           </div>
         </div>
         <Link href="/user" aria-label="보관함">
