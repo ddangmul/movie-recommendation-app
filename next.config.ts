b6d244127c7ee9f570432f2ffba4051a49e59d3e
ddangmul <j.ddangmul@gmail.com>
2025-05-14 16:08:01 +0900

    RootLayout, MainHeader, Footer 작성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
---
+++ src/components/footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import MainLogo from "@/src/assets/MainLogo.png";
+import { Twitter, Facebook, Instagram } from "lucide-react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full bg-[#202020] text-[#dbdbdb] shadow py-8 fixed bottom-0 left-0 z-50">
+      <div className="w-full flex items-center justify-between md:px-6 lg:px-60 text-xs">
+        <div className="flex flex-col gap-2">
+          <p className="space-x-3">
+            <Link href="/" aria-label="서비스 이용약관">
+              서비스 이용약관
+            </Link>
+            <Link href="/" aria-label="개인정보 처리방침">
+              개인정보 처리방침
+            </Link>
+          </p>
+          <div>
+            <p>주식회사 릴픽 | 대표 OOO | 서울특별시 종로구 </p>
+            <p>사업자 등록 번호 000-00-00000</p>
+          </div>
+          <p className="flex gap-2">© REELPICK, Inc. All rights reserved.</p>
+        </div>
+        <div className="flex gap-2">
+          <Instagram aria-label="인스타그램" />
+          <Facebook aria-label="페이스북" />
+          <Twitter aria-label="X (트위터)" />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
