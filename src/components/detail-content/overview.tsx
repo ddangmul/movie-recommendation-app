5be36fb27105bca52f54600494b04ee1968c97d8
ddangmul <j.ddangmul@gmail.com>
2025-05-15 12:46:55 +0900

    tailwind line-clamp overview에 적용

--- src/components/detail-content/overview.tsx
+++ src/components/detail-content/overview.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { PlusIcon } from "lucide-react";
 import Rating from "../rating";
+import { useState } from "react";
 
 type Props = {
   content: any;
@@ -8,6 +11,8 @@ type Props = {
 };
 
 export default function OverviewSection({ content, overview }: Props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <section className="flex gap-4 items-start">
       <Image
@@ -18,14 +23,34 @@ export default function OverviewSection({ content, overview }: Props) {
         className="flex-shrink-0"
       />
       <div className="w-full flex flex-col justify-between px-2 max-h-[300px] overflow-y-auto">
-        <div className="flex flex-col items-end gap-2 py-2">
+        <div className="flex flex-col items-end gap-4 py-2">
           <div>
             <Rating />
-            <p className="text-xs text-right text-gray-600">평가하기</p>
+            <p className="text-xs text-right text-gray-600 mt-2">평가하기</p>
           </div>
-          <PlusIcon className="mb-4" />
+          <div className="mb-10 flex flex-col items-end">
+            <PlusIcon className="text-gray-600 " />
+            <p className="text-xs text-right text-gray-600 mt-1">보고싶어요</p>
+          </div>
         </div>
-        <div className="text-sm overflow-y-auto">{overview}</div>
+        <div className="text-sm text-gray-800 relative">
+          {" "}
+          <p
+            className={`${
+              isExpanded ? "" : "line-clamp-4"
+            } transition-all duration-300`}
+          >
+            {overview || "줄거리 정보가 없습니다."}
+          </p>
+          {overview.length > 100 && (
+            <button
+              onClick={() => setIsExpanded(!isExpanded)}
+              className="text-gray-500 text-xs mt-1 hover:underline"
+            >
+              {isExpanded ? "접기" : "더보기"}
+            </button>
+          )}
+        </div>
       </div>
     </section>
   );
--- src/components/rating.tsx
+++ src/components/rating.tsx
@@ -5,14 +5,14 @@ import { Star } from "lucide-react";
 
 export default function Rating() {
   const [rating, setRating] = useState(0); // 0~5까지 별점 저장
-  const [hover, setHover] = useState(0);
+  const [_hover, setHover] = useState(0);
 
   return (
     <div className="flex gap-1">
       {[1, 2, 3, 4, 5].map((star) => (
         <Star
           key={star}
-          size={20}
+          size={30}
           className={`cursor-pointer ${
             star <= rating ? "text-yellow-400 fill-yellow-400" : "text-gray-400"
           }`}
