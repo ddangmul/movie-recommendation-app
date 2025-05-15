495b295a7c8813db2f0731136149939d7731b4ff
ddangmul <j.ddangmul@gmail.com>
2025-05-15 22:31:06 +0900

    searchpage media_type에 따라 동적 렌더링

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -11,6 +11,7 @@ export default function SearchPage() {
   const searchParams = useSearchParams();
   const searchTerm = searchParams.get("q") || "";
   const [searchResult, setSearchResult] = useState([]);
+  const [mediaType, setMediaType] = useState<"all" | "movie" | "tv">("all");
 
   useEffect(() => {
     if (!searchTerm) return;
@@ -28,16 +29,53 @@ export default function SearchPage() {
     fetchSearchTerm();
   }, [searchTerm]);
 
+  const filteredResults =
+    mediaType === "all"
+      ? searchResult
+      : searchResult.filter((item: any) => item.media_type === mediaType);
+
   const handleClick = (content: any) => {
     router.push(`/${content.media_type}/${content.id}`);
   };
 
   return (
-    <div className="pt-28 px-4 md:px-6 lg:px-60 min-h-screen">
-      <h2 className="text-lg px-2">'{searchTerm}' 검색 결과</h2>
+    <div className="pt-24 px-4 md:px-6 lg:px-60 min-h-screen">
+      <h2 className="text-lg px-2 my-4">'{searchTerm}' 검색 결과</h2>
+      <div className="px-2 flex gap-4 mb-4">
+        <button
+          className={`px-4 py-1 rounded-md ${
+            mediaType === "all"
+              ? "bg-[#303030] text-white"
+              : "bg-[#f2f2f2] text-[#3c3c3c]"
+          }`}
+          onClick={() => setMediaType("all")}
+        >
+          전체
+        </button>
+        <button
+          className={`px-4 py-1 rounded-md ${
+            mediaType === "movie"
+              ? "bg-[#303030] text-white"
+              : "bg-[#f2f2f2] text-[#3c3c3c]"
+          }`}
+          onClick={() => setMediaType("movie")}
+        >
+          영화
+        </button>
+        <button
+          className={`px-4 py-1 rounded-md ${
+            mediaType === "tv"
+              ? "bg-[#303030] text-white"
+              : "bg-[#f2f2f2] text-[#3c3c3c]"
+          }`}
+          onClick={() => setMediaType("tv")}
+        >
+          시리즈
+        </button>
+      </div>
       <section className="mt-4">
         <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-3 lg:gap-10">
-          {searchResult.map((content: any) => (
+          {filteredResults.map((content: any) => (
             <li
               key={content.poster_path}
               className="mt-6 mx-0"
